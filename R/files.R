f304f916bcc7909623589e7e38949ff04b0a9f98
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-20 13:01:43 +0200

    - Add `print()` method.

--- R/files.R
+++ R/files.R
@@ -23,3 +23,32 @@ pf_classify <- function(files) {
 new_pkgfiles <- function(x) {
   structure(x, class = c("pkgfiles", class(x)))
 }
+
+#' @export
+print.pkgfiles <- function(x, ...) {
+  class <- forcats::fct_drop(factor(x[["class"]], levels = forcats::fct_inorder(classification[["class"]])))
+  xs <- split(x, class)
+
+  nrows <- map_int(xs, nrow)
+  descs <- classification[["desc"]][match(names(nrows), classification[["class"]])]
+
+  unclass <- which(is.na(class))
+  if (has_length(unclass)) {
+    nrows <- c(nrows, length(unclass))
+    descs <- c(descs, paste0("Other: ", comma_list(crayon::magenta(x[["path"]][unclass]))))
+  }
+
+  cli::cat_bullet(crayon::green(format(nrows)), " ", descs)
+
+  invisible(x)
+}
+
+comma_list <- function(x) {
+  MAX_LIST <- 6
+  if (length(x) > MAX_LIST) {
+    length(x) <- MAX_LIST
+    x[[MAX_LIST]] <- cli::symbol$ellipsis
+  }
+
+  paste(x, collapse = ", ")
+}
